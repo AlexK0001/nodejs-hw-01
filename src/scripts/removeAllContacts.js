2e935490e2d319f0bd8ba2e2dd3364320abb28ce
Alexandr <alexandr.kopyletz@gmail.com>
2024-10-15 15:21:52 +0300

    15.10

--- src/scripts/removeAllContacts.js
+++ src/scripts/removeAllContacts.js
@@ -1,3 +1,14 @@
-export const removeAllContacts = async () => {};
+import writeContacts from '../utils/writeContacts.js';
+
+export const removeAllContacts = async () => {
+    try {
+        const emptyContacts = [];
+        await writeContacts(emptyContacts);
+        console.log('Усі контакти успішно видалено!');
+
+    } catch (error) {
+        console.error(`Error adding contact: ${error.message}`);
+    }
+};
 
 removeAllContacts();
--- src/scripts/removeLastContact.js
+++ src/scripts/removeLastContact.js
@@ -1,3 +1,20 @@
-export const removeLastContact = async () => {};
+import writeContacts from '../utils/writeContacts.js';
+import readContacts from '../utils/readContacts.js';
+
+export const removeLastContact = async () => {
+    try {
+        const allContacts = await readContacts();
+        if (allContacts.length>=1) {
+            const newContacts = allContacts.pop();
+            await writeContacts(newContacts);
+            console.log('Останній контакт успішно видалено!');
+        } else {
+            console.log('Контактів не знайдено!');
+        }
+
+    } catch (error) {
+        console.error(`Error adding contact: ${error.message}`);
+    }
+};
 
 removeLastContact();
